7bf0626866c4349c48336a01fec84d56b5b61d71
SaeedAmini <saeedamini506750@gmail.com>
2026-03-29 15:35:17 +0430

    edit

--- smartcar/settings.py
+++ smartcar/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT=os.path.join(BASE_DIR,'static_cdn')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media_cdn')
 
 STATICFILES_DIRS=[
